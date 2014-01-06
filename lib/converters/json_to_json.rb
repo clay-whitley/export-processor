dde7a38e31d01c30da6a575a116ef052bb22b59f
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-01-06 11:59:59 -0800

    add commands for core features

---
+++ lib/converters/json_to_json.rb
@@ -0,0 +1,16 @@
+require "json"
+ 
+files = Dir["*.json"]
+ 
+result = []
+ 
+files.each do |file|
+  json = File.open(file, "r")
+  json.readlines.each do |line|
+    result << JSON.parse(line.chomp)
+  end
+end
+ 
+output = File.open(Time.now.to_i.to_s + "_result.json", "w+")
+ 
+output.write(JSON.pretty_generate(result))
