174c02d2c0e0d01ea184206ecc889c9a2c655466
raorao <savinirs@gmail.com>
2014-01-06 14:47:31 -0800

    Reimporter now initializes KMTS
    - cron option turned to false for now, may later make sense to switch

--- lib/reimporter.rb
+++ lib/reimporter.rb
@@ -1,7 +1,11 @@
+require 'kmts'
+
 module KMExport
   class Reimporter
     def send_to_KM json, key
-
+      KMTS.init(key,
+        :use_cron => false, 
+        :to_stderr => true)
     end
   end
 end
