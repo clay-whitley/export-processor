709f6a6d69966693472cf5e4a99f8341df45ae1a
raorao <savinirs@gmail.com>
2014-01-07 09:50:10 -0800

    send_line_to_KM now explicitly sets the _d value
    - _d tells to KM to manually set the timestamp with the provided
    _t value. While it was working without _d being explicitly set,
    it makes sense to do so for clarity

--- lib/reimporter.rb
+++ lib/reimporter.rb
@@ -23,6 +23,7 @@ module KMExport
       identity = line.delete("_p")
       second_identity = line.delete("_p2")
       event = line.delete("_n")
+      line["_d"] = 1
 
       KMTS.alias(identity,second_identity) if second_identity
       KMTS.record(identity,event,log) if event
