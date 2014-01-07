c4bb39f6a6ff6936544d5ae392c439262175d166
raorao <savinirs@gmail.com>
2014-01-06 16:07:09 -0800

    send_to_KM now sends data to KISSmetrics
    - send_line_to_KM evaluates presence of second_identity and event
    in order to ensure correct alias/record/set call is made
    - prints to stdout on completion of line/file

--- lib/reimporter.rb
+++ lib/reimporter.rb
@@ -1,11 +1,30 @@
 require 'kmts'
+require 'json'
 
 module KMExport
   class Reimporter
-    def send_to_KM json, key
+    def send_to_KM(json, key)
       KMTS.init(key,
         :use_cron => false, 
         :to_stderr => true)
+
+      logs = JSON.parse(json)
+      logs.each do |line|
+        send_line_to_KM(line)
+        @stdout.print('.')
+      end
+
+      @stdout.print('Reimportation process completed.')
+    end
+
+    def send_line_to_KM(line)
+      identity = line.delete("_p")
+      second_identity = line.delete("_p2")
+      event = line.delete("_n")
+
+      KMTS.alias(identity,second_identity) if second_identity
+      KMTS.record(identity,event,log) if event
+      KMTS.set(identity,log) unless event || second_identity
     end
   end
 end
