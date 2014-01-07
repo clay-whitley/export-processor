b7165a3cafef3c8aaf7d5227ab0f53adae500d82
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-01-07 13:28:42 -0800

    implement alias parser

---
+++ lib/alias_parser.rb
@@ -0,0 +1,20 @@
+module KMExport
+  def self.alias_parser(jsonfile)
+    file = File.open(jsonfile)
+    alias_file = File.open(Time.now.to_i.to_s + "_aliases.json", "w+")
+    json = JSON.parse(file.read)
+    aliases = []
+    data = []
+
+    json.each do |row|
+      if row["_p2"]
+        aliases << row
+      else
+        data << row
+      end
+    end
+    new_file = File.open(jsonfile, "w+")
+    new_file.write(JSON.pretty_generate(data))
+    alias_file.write(JSON.pretty_generate(aliases))
+  end
+end
--- lib/km-export-processor.rb
+++ lib/km-export-processor.rb
@@ -8,3 +8,4 @@ end
 require_relative 'converters/json_to_json'
 require_relative 'converters/json_to_csv'
 require_relative 'reimporter'
+require_relative 'alias_parser'
