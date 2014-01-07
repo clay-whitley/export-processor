3179a52d1011e700ce69791ef0dd20e062ccae13
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-01-06 16:34:58 -0800

    finish integrating json_to_json converter

--- lib/converters/json_to_json.rb
+++ lib/converters/json_to_json.rb
@@ -1,16 +1,18 @@
-require "json"
+module KMExport
+  def self.json_to_json
+    files = Dir["*.json"]
  
-files = Dir["*.json"]
- 
-result = []
- 
-files.each do |file|
-  json = File.open(file, "r")
-  json.readlines.each do |line|
-    result << JSON.parse(line.chomp)
+    result = []
+     
+    files.each do |file|
+      json = File.open(file, "r")
+      json.readlines.each do |line|
+        result << JSON.parse(line.chomp)
+      end
+    end
+     
+    output = File.open(Time.now.to_i.to_s + "_result.json", "w+")
+     
+    output.write(JSON.pretty_generate(result))
   end
 end
- 
-output = File.open(Time.now.to_i.to_s + "_result.json", "w+")
- 
-output.write(JSON.pretty_generate(result))
