b152922a165f9c097a1630c1de286789cbe0d31e
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-01-08 21:17:17 -0800

    json_to_csv test

---
+++ spec/features/json_to_csv_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "json_to_csv" do
+  before do
+    @testfilename = Time.now.to_i.to_s + "_test.json"
+    testfile = File.open(@testfilename, "w+")
+    json = JSON.pretty_generate(
+      [
+        {test: "test"},
+        {test2: "test2"},
+        {test3: "test3", test: "test3"},
+        {test: "test"},
+        {test2: "test2"},
+        {test3: "test3", test: "test3"}
+      ]
+      )
+    testfile.write(json)
+    testfile.close
+  end
+
+  it "correctly converts JSON file to a CSV file" do
+    KMExport.json_to_csv(@testfilename)
+    @csv_files = Dir["*converter_result.csv"]
+    @csv_files.length == 1
+  end
+
+  after do
+    File.delete(@testfilename)
+    File.delete(@csv_files.first)
+  end
+end
